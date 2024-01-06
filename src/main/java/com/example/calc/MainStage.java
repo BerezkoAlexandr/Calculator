d1301221ebcf8b1f30bbe4e0ec37c667c4556a97
BerezkoAlexandr <alexbiker@mail.ru>
2024-01-06 15:10:28 +0300

    Initial commit

---
+++ src/main/java/com/example/calc/MainStage.java
@@ -0,0 +1,20 @@
+package com.example.calc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainStage extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
